a9ce9b3eac36055a98df1391c7c9fc6e47f18e73
Ба3рон <andrey__ch@mail.ru>
2017-04-23 12:02:41 +0300

    add lint package.json

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 /** Created by Барон on 18.04.2017.*/
-var web = function (){
-
-    console.log('web');
+var Web = function() {
+    // V console.log('web');
 };
 
-var ii = new web();
+var ii = new Web();
+// G var tt = 'hh';
+
+ii.get = function() {
+    tt = 'ee';
+};
+tt = 'ww';
